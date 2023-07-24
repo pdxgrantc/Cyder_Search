cd26ed70d8cc44b41f2a6ceecd33cd8c44e119a3
pdxgrantc <pdxgrantc@gmail.com>
2023-07-24 20:43:20 +0000

    push

--- back_end/server.js
+++ back_end/server.js
@@ -1,14 +1,22 @@
-// app.js
 const express = require('express');
+const serveStatic = require('serve-static');
 const app = express();
 const port = 3000; // Change this to the desired port number (e.g., 8000, 8080, etc.)
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// Serve static files from the "front_end/build" directory with custom MIME types
+app.use(serveStatic('front_end/build', { 
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    } else if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
 
 // Route handling
 app.get('/', (req, res) => {
-  res.sendFile('../front_end/build/index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname + '/../front_end/build' });
 });
 
 // Start the server

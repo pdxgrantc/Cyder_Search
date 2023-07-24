8df86e6cbca79fea797d03d2cfb0c1f83b0d7d53
wrightqwcw <wrightq@oregonstate.edu>
2023-07-24 14:17:03 -0700

    format

--- python_api/index.js
+++ python_api/index.js
@@ -1,7 +1,10 @@
 
 
 variable_to_pass = {
-    'i:label__contains': computer, 'i:fqdn__contains': 'forestry.oregonstate.edu', 'e:fqdn__contains': 'DOCK', 'e:fqdn__contains': 'dock'
+    'i:label__contains': computer, 
+    'i:fqdn__contains': 'forestry.oregonstate.edu', 
+    'e:fqdn__contains': 'DOCK', 
+    'e:fqdn__contains': 'dock'
 }
 
 // code to pass to python

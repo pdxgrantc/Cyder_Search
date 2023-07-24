432c0b9e70eec4b1cc020571f6b30c2a1bc7dd3f
pdxgrantc <pdxgrantc@gmail.com>
2023-07-24 21:07:29 +0000

    python stuff

--- back_end/server.js
+++ back_end/server.js
@@ -1,12 +1,3 @@
-// write a node js server that listens on port 3000 and serves a react app at ../front_end/build/index.html
-// use express and path modules
-// use express.static to serve the static files in ../front_end/build
-// use app.get to serve the index.html file
-// use app.listen to listen on port 3000
-
-
-// Path: back_end/server.js
-
 const express = require('express');
 const path = require('path');
 
---
+++ python_api/index.js
@@ -0,0 +1,5 @@
+variable_to_pass = {
+    'variable': 'value'
+}
+
+// code to pass to python

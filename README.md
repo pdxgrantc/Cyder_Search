2e5c77882d4a18a13071cfa6ea40b81220ccc1fc
pdxgrantc <pdxgrantc@gmail.com>
2023-07-25 15:57:13 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Forestry Helpdesk Cyder Search Site
+
+This site is designed to use the Cyder api to offer a more feature rich search functionality for Cyder.
+
+Architecture:
+The front end is built with React JS and the backend is built with Node JS. The backend is a simple express server that serves the website as well as the routing necissary to interface with a python api calling library to interface with Cyder. 
+
+Versions:
+Node: 18
+React: 16.13.1
--- front_end/src/components/Home/Home.js
+++ front_end/src/components/Home/Home.js
@@ -22,6 +22,14 @@ function Input() {
     const [name, setName] = useState('')
     const [fqdn, setFqdn] = useState('')
     const [ctnr, setCtnr] = useState('')
+    const [poNumber, setPoNumber] = useState('')
+    const [serialNumber, setSerialNumber] = useState('')
+    const [location, setLocation] = useState('')
+    const [hardwareType, setHardwareType] = useState('')
+
+    // static interface features
+    const[ipAddr, setIpAddr] = useState('')
+    const[macAddr, setMacAddr] = useState('')
 
     const handleCtnrChange = (event) => {
         setCtnr(event.target.value)
@@ -32,6 +40,12 @@ function Input() {
         setName('')
         setFqdn('')
         setCtnr('')
+        setPoNumber('')
+        setSerialNumber('')
+        setLocation('')
+        setHardwareType('')
+        setIpAddr('')
+        setMacAddr('')
     }
 
     const handleSubmit = (event) => {
@@ -41,7 +55,13 @@ function Input() {
             id: id,
             name: name,
             fqdn: fqdn,
-            ctnr: ctnr
+            ctnr: ctnr,
+            poNumber: poNumber,
+            serialNumber: serialNumber,
+            location: location,
+            hardwareType: hardwareType,
+            ipAddr: ipAddr,
+            macAddr: macAddr,
         }
 
         console.log(data)
@@ -103,6 +123,66 @@ function Input() {
                     value={fqdn}
                     onChange={(e) => setFqdn(e.target.value)}
                     id="fqdn"
+                />
+            </div>
+            <div>
+                <input
+                    type='text'
+                    className='w-[50vw] h-[2rem] rounded bg-input_bg_color text-text_white px-[1rem] py-[.5rem] text-small'
+                    placeholder="PO Number"
+                    value={poNumber}
+                    onChange={(e) => setPoNumber(e.target.value)}
+                    id="poNumber"
+                />
+            </div>
+            <div>
+                <input
+                    type='text' 
+                    className='w-[50vw] h-[2rem] rounded bg-input_bg_color text-text_white px-[1rem] py-[.5rem] text-small'
+                    placeholder="Serial Number"
+                    value={serialNumber}
+                    onChange={(e) => setSerialNumber(e.target.value)}
+                    id="serialNumber"
+                />
+            </div>
+            <div>
+                <input
+                    type='text' 
+                    className='w-[50vw] h-[2rem] rounded bg-input_bg_color text-text_white px-[1rem] py-[.5rem] text-small'
+                    placeholder="Location"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    id="location"
+                />
+            </div>
+            <div>
+                <input
+                    type='text'
+                    className='w-[50vw] h-[2rem] rounded bg-input_bg_color text-text_white px-[1rem] py-[.5rem] text-small'
+                    placeholder="Hardware Type"
+                    value={hardwareType}
+                    onChange={(e) => setHardwareType(e.target.value)}
+                    id="hardwareType"
+                />
+            </div>
+            <div>
+                <input
+                    type='text'
+                    className='w-[50vw] h-[2rem] rounded bg-input_bg_color text-text_white px-[1rem] py-[.5rem] text-small'
+                    placeholder="IP Address"
+                    value={ipAddr}
+                    onChange={(e) => setIpAddr(e.target.value)}
+                    id="ipAddr"
+                />
+            </div>
+            <div>
+                <input
+                    type='text'
+                    className='w-[50vw] h-[2rem] rounded bg-input_bg_color text-text_white px-[1rem] py-[.5rem] text-small'
+                    placeholder="MAC Address"
+                    value={macAddr}
+                    onChange={(e) => setMacAddr(e.target.value)}
+                    id="macAddr"
                 />
             </div>
             <div>

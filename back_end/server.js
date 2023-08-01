64ce932ca7726a1eaedf506fb98f52b85ca73dda
pdxgrantc <pdxgrantc@gmail.com>
2023-08-01 15:52:23 +0000

    push

--- back_end/server.js
+++ back_end/server.js
@@ -8,38 +8,14 @@ const start = require('../api_interface/lib.js')
 const util = require('util');
 const call_api = require('../api_interface/lib.js');
 
-
 const app = express();
-
-testOBJ = {
-  "count": 1,
-  "next": null,
-  "previous": null,
-  "results": [{
-    "systemav_set": [{ "id": "https://cyder.oregonstate.edu/core/system/attributes/213305/", "attribute": "Department", "value": "FCR" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213306/", "attribute": "Hardware type", "value": "Optiplex 7050" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213307/", "attribute": "Location", "value": "RH 313" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213308/", "attribute": "Operating system", "value": "Windows 10" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213309/", "attribute": "Owning unit", "value": "FCR" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213310/", "attribute": "PO number", "value": "P0106250" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213311/", "attribute": "Purchase date", "value": "06/07/2017" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213312/", "attribute": "Serial number", "value": "3Y7HKH2" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213313/", "attribute": "User ID", "value": "Paul Foshay" },
-    { "id": "https://cyder.oregonstate.edu/api/v1/core/system/attributes/213314/", "attribute": "Warranty date", "value": "06/07/2021" }],
-    "ctnr": "https://cyder.oregonstate.edu/api/v1/core/ctnr/70/",
-    "id": 107680,
-    "created": "2017-06-19T16:19:05",
-    "modified": "2020-03-18T10:27:29",
-    "name": "PATHOS"
-  }]
-}
 
 // Parse JSON bodies for incoming requests
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../front_end/build')));
 
-app.get('/api/search', async (req, res) => {
+app.post('/api/search', async (req, res) => {
   const data = req.body;
 
   var query = createQuery(data);
@@ -56,12 +32,44 @@ app.get('/api/search', async (req, res) => {
       console.log(result)
       // send the json as data.results
       res.json(result);
-      })
+    })
     .catch((error) => {
       console.error('Error occurred t:', error.message);
       res.status(500).json({ error: error.message }); // Send an error response to the client.
     });
 });
+
+app.get('/api/search', async (req, res) => {
+  // Retrieve the query parameters from the request
+  const { NAME, NAMEU, PO, POU, SN, SNU, LOC, LOCU, HT, HTU, IP, IPU, MAC, MACU } = req.query;
+
+  console.log(HTU)
+
+  // Build the query object
+  const query = JSON.parse(JSON.stringify(build_query(NAME, NAMEU, PO, POU, SN, SNU, LOC, LOCU, HT, HTU, IP, IPU, MAC, MACU)));
+
+  console.log(query);
+
+  await call_api(query)
+    .then((result) => {
+      // pasrse
+      result = JSON.stringify(result);
+      result = JSON.parse(result);
+
+      console.log(result)
+      // send the json as data.results
+      res.json(result);
+    })
+    .catch((error) => {
+      console.error('Error occurred t:', error.message);
+      res.status(500).json({ error: error.message }); // Send an error response to the client.
+    });
+      // Send the response back to the client
+      
+
+  // Return a response (you can customize this as per your needs)
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../front_end/build/index.html'));
@@ -71,6 +79,54 @@ app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
 
+
+function build_query(NAME, NAMEU, PO, POU, SN, SNU, LOC, LOCU, HT, HTU, IP, IPU, MAC, MACU) {
+  let query = {
+  }
+  // add on the name if it exists
+  if (NAME !== '') {
+    query.name = NAME;
+  }
+  // for each of these fields if they exist add them to the query
+  // or if the user specified that they want to search for empty fields
+  if (PO !== '') {
+    query.po_number = PO;
+  }
+  else if (POU === "true") {
+    query.po_number = "";
+  }
+  if (SN !== '') {
+    query.serial_number = SN;
+  }
+  else if (SNU === "true") {
+    query.serial_number = "";
+  }
+  if (LOC !== '') {
+    query.location = LOC;
+  }
+  else if (LOCU === "true") {
+    query.location = "";
+  }
+  if (HT !== '') {
+    query.hardware_type = HT;
+  }
+  else if (HTU === "true") {
+    query.hardware_type = "";
+  }
+  // add on the ip and mac if they exist
+  if (IP !== '') {
+    query.ip_str = IP;
+  }
+  if (MAC !== '') {
+    query.mac_addr = MAC;
+  }
+
+  return query;
+}
+
+
+
+/*
 function createQuery(data) {
   let query = {
   }
@@ -101,3 +157,4 @@ function createQuery(data) {
 
   return query;
 }
+*/
